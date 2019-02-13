0749e12601fbf05dcd5234f328a507d49022987d
Spassu Tecnologia <Spassu Tecnologia@POS1A723B14W>
2019-02-13 17:04:27 -0300

    Salvar aluno no banco de dados e alterar um aluno já salvo

---
+++ src/br/com/loucademia/application/service/AlunoService.java
@@ -0,0 +1,43 @@
+package br.com.loucademia.application.service;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+
+import br.com.loucademia.application.util.StringUtils;
+import br.com.loucademia.application.util.Validation;
+import br.com.loucademia.domain.aluno.Aluno;
+import br.com.loucademia.domain.aluno.AlunoRepository;
+
+@Stateless
+public class AlunoService {
+	
+	@EJB
+	private AlunoRepository alunoRepository;
+	
+	public void createOrUpdate(Aluno aluno) {
+		if(StringUtils.isEmpty(aluno.getMatricula()))
+				create(aluno);
+		else
+			update(aluno);
+	}
+	
+	private void create(Aluno aluno) {
+		Validation.assertNotEmpty(aluno);
+		
+		String maxMatricula = alunoRepository.getMaxMatriculaAno();
+		
+		aluno.gerarMatricula(maxMatricula);
+		alunoRepository.store(aluno);
+	}
+	
+	private void update(Aluno aluno) {
+		Validation.assertNotEmpty(aluno);
+		Validation.assertNotEmpty(aluno.getMatricula());
+		alunoRepository.update(aluno);
+	}
+	
+	public Aluno findByMatricula(String matricula) {
+		return alunoRepository.findByMatricula(matricula);
+	}
+
+}
--- src/br/com/loucademia/interfaces/aluno/web/AlunoBean.java
+++ src/br/com/loucademia/interfaces/aluno/web/AlunoBean.java
@@ -2,19 +2,43 @@ package br.com.loucademia.interfaces.aluno.web;
 
 import java.io.Serializable;
 
+import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 import javax.inject.Named;
 
+import br.com.loucademia.application.service.AlunoService;
+import br.com.loucademia.application.util.StringUtils;
 import br.com.loucademia.domain.aluno.Aluno;
 
 @Named
 @RequestScoped
 public class AlunoBean implements Serializable{
 	
+	@EJB
+	private AlunoService alunoService;
+	
+	@Inject
+	private FacesContext facesContext;
+	
 	private Aluno aluno = new Aluno();
 	
+	private String matricula;
+	
+	private String titulo = "Novo aluno";
+	
+	public void carregar() {
+		if(!StringUtils.isEmpty(matricula)) {
+			aluno = alunoService.findByMatricula(matricula);
+			titulo = "Alterar aluno";
+		}
+	}
+	
 	public String gravar() {
-		System.out.println("ALUNO ==> " + aluno);
+		alunoService.createOrUpdate(aluno);
+		facesContext.addMessage(null, new FacesMessage("Dados gravados com sucesso!"));
 		return null;
 	}
 	
@@ -24,5 +48,21 @@ public class AlunoBean implements Serializable{
 	
 	public void setAluno(Aluno aluno) {
 		this.aluno = aluno;
+	}
+	
+	public String getMatricula() {
+		return matricula;
+	}
+	
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
 	}
 }
